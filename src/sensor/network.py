f12c5cda871a494ef349ffb617eba4c8e726b45b
feliconut <feliconut@outlook.com>
2020-01-07 12:02:10 +0800

    new lib: pynetworktables

--- src/sensor/network.py
+++ src/sensor/network.py
@@ -1,11 +1,36 @@
+# DOC: https://pynetworktables.readthedocs.io/en/latest/api.html#networktables.NetworkTable.getBoolean
+#! pip install pynetworktables
 from ..default import *
-
+from networktables import NetworkTables
+import threading
 
 class NetworkTable(ModuleDynamic):
     def __init__(self):
         ModuleDynamic.__init__(self)
+        self.table = None
 
     def initialize(self):
+        cond = threading.Condition()
+        notified = [False]
+
+
+        def connectionListener(connected, info):
+            print(info, '; Connected=%s' % connected)
+            with cond:
+                notified[0] = True
+                cond.notify()
+
+
+        NetworkTables.initialize(server='10.80.15.2')
+        NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+        with cond:
+            print("Waiting")
+            if not notified[0]:
+                cond.wait()
+
+        print("Connected!")
+        self.table = NetworkTables.getTable('datatable')
         return True
 
     async def work(self):
